461adb44dafbc890eb3ac748afb96e0fd10806c7
Bin Le <bin.le.code@gmail.com>
2021-01-19 20:51:18 -0500

    initial load

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Getting Started with Create React App
+
+## dev log
+
+ref: https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_todo_list_beginning
+
+ref code repo: https://github.com/mdn/todo-react
+
+
+## bootstrap
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/components/Todo.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Todo(props) {
+    const [isEditing, setEditing] = useState(false);
+    const [newName, setNewName] = useState('');
+
+    // create ref for elements for cursor current.focus call
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
+
+    const editingTemplate = (
+        <form className="stack-small" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="todo-label" htmlFor={props.id}>
+                    New name for {props.name}
+                </label>
+                <input id={props.id} className="todo-text" type="text"
+                       value={newName}
+                       onChange={handleChange}
+                       onKeyDown={handleKeyDown}
+                       ref={editFieldRef}
+                />
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn todo-cancel"
+                        onClick={() => setEditing(false)}
+                >
+                    Cancel
+                    <span className="visually-hidden">renaming {props.name}</span>
+                </button>
+                <button type="submit" className="btn btn__primary todo-edit">
+                    Save
+                    <span className="visually-hidden">new name for {props.name}</span>
+                </button>
+            </div>
+        </form>
+    );
+
+    const viewTemplate = (
+        <div className="stack-small">
+            <div className="c-cb">
+                <input id={props.id} type="checkbox"
+                       defaultChecked={props.completed}
+                       onChange={() => props.toggleTaskCompleted(props.id)}
+                />
+                <label className="todo-label" htmlFor={props.id}>
+                    {props.name}
+                </label>
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn"
+                        onClick={() => setEditing(true)}
+                        ref={editButtonRef}
+                >
+                    Edit <span className="visually-hidden">{props.name}</span>
+                </button>
+                <button type="button" className="btn btn__danger"
+                        onClick={() => props.deleteTask(props.id)}
+                >
+                    Delete <span className="visually-hidden">{props.name}</span>
+                </button>
+            </div>
+        </div>
+    );
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.editTask(props.id, newName);
+        // reset new name value
+        setNewName('');
+        // exit editing mode
+        setEditing(false);
+    }
+
+    function handleChange(e) {
+        setNewName(e.target.value);
+    }
+
+    // catch esc key to exit editing mode without saving
+    // ! NOTE for some reason onKeyPress does not capture escape key (code 27)
+    function handleKeyDown(e) {
+        if (e.keyCode === 27) {
+            if (isEditing) {
+                setEditing(false);
+            }
+        }
+    }
+
+    // set current state to given value but return its previous state
+    function usePrevious(value) {
+        const ref = useRef();
+        // useEffect invokes callback after main flow, aka the 'return' statement below
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    }
+
+    const wasEditing = usePrevious(isEditing);
+
+    // * useEffect() takes a function as an argument,
+    // * this function is executed **after** the component renders
+    // ! NOTE the second argument is a list of values useEffect() should depend on
+    useEffect(() => {
+        if (!wasEditing && isEditing) {
+            editFieldRef.current.focus();
+        }
+        if (wasEditing && !isEditing) {
+            editButtonRef.current.focus();
+        }
+    }, [isEditing, wasEditing]);
+
+    return (
+        <li className="todo stack-small">
+            {isEditing ? editingTemplate : viewTemplate}
+        </li>
+    );
+}
+
